c87d2b333df21faa837ed038c0c7c853b3353a63
gregoryjardina <harjuna.porto@gmail.com>
2025-04-20 22:28:36 -0300

    Inserção inicial

---
+++ candidatura/src/ProcessoSeletivo.java
@@ -0,0 +1,15 @@
+public class ProcessoSeletivo {
+    public static void main(String[] args)  {
+        System.out.println("Processo Seletivo");
+    }
+    static void analisarCandidato(double salarioPretendido){
+        double salarioBase = 2000.00;
+        if (salarioBase > salarioPretendido){
+            System.out.println("Ligar para o candidato.");
+        }else if (salarioBase == salarioPretendido){
+            System.out.println("Aguardando demais candidatos.");
+        }else{
+            System.out.println("Ligar para o candidato com contrarpoposta!");
+        }
+    }
+}
